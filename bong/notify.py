5afee6bf73c5fef83a41d7e6c4f4fc94156157ad
Alistair Lynn <arplynn@gmail.com>
2015-01-28 10:49:37 +0000

    Port bong to Darwin

--- bong/notify.py
+++ bong/notify.py
@@ -1,4 +1,11 @@
 import subprocess
+import sys
 
-def notify(message):
-    subprocess.check_call(['notify-send', message])
+def notify(message, sys=sys):
+    if sys.platform == 'linux':
+        subprocess.check_call(['notify-send', message])
+    elif sys.platform == 'darwin':
+        subprocess.check_call(['terminal-notifier', '-message', message])
+    else:
+        print(message)
+
--- tests/test_notify.py
+++ tests/test_notify.py
@@ -1,9 +1,32 @@
 from bong.notify import notify
+import sys
+from io import StringIO
 
 from unittest.mock import patch
+from nose.tools import eq_
+
+class FakeSys(object):
+    def __init__(self, **kwargs):
+        self._fake = kwargs
+
+    def __getattr__(self, name):
+        try:
+            return self._fake[name]
+        except KeyError:
+            return getattr(sys, name)
 
 @patch('subprocess.check_call')
-def test_notify(system):
-    notify('bees')
+def test_notify_linux(system):
+    notify('bees', sys=FakeSys(platform='linux'))
     system.assert_called_with(['notify-send', 'bees'])
+
+@patch('subprocess.check_call')
+def test_notify_darwin(system):
+    notify('bees', sys=FakeSys(platform='darwin'))
+    system.assert_called_with(['terminal-notifier', '-message', 'bees'])
+
+def test_notify_other():
+    with patch('sys.stdout', new=StringIO()) as fake_out:
+        notify('bees', sys=FakeSys(platform='aix'))
+        eq_(fake_out.getvalue(), 'bees\n')
 
