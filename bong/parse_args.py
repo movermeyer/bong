7c0c349656e6f02be0f3f0044f5d225f3688be08
Alistair Lynn <arplynn@gmail.com>
2015-02-15 21:09:49 +0000

    Clean up the argument parsing

--- bong/parse_args.py
+++ bong/parse_args.py
@@ -3,21 +3,22 @@ from .metadata import VERSION, SUMMARY
 import argparse
 
 PARSER = argparse.ArgumentParser(description=SUMMARY)
-PARSER.add_argument('-V', '--version', action='version', version=VERSION,
-                    help='Show version')
+PARSER.add_argument('-V', '--version', action='version',
+                    version='%(prog)s {}'.format(VERSION),
+                    help='show version')
 PARSER.add_argument('-s', '--short-break', action='store_const', const=5,
                     dest='minutes', default=25,
-                    help='Time for a Pomodoro system short break')
+                    help='time for a Pomodoro system short break')
 PARSER.add_argument('-l', '--long-break', action='store_const',
                     const=15, dest='minutes',
-                    help='Time for a Pomodoro system long break')
+                    help='time for a Pomodoro system long break')
 PARSER.add_argument('-p', '--pomodoro', action='store_const',
                     const=25, dest='minutes',
-                    help='Time for a Pomodoro system single Pomodoro')
+                    help='time for a Pomodoro system single Pomodoro')
 PARSER.add_argument('-t', '--time', action='store', type=int, dest='minutes',
-                    help='Timer length, in minutes')
+                    help='timer length, in minutes')
 PARSER.add_argument('-m', '--message', default=DEFAULT_MESSAGE,
-                    help='Message to display in the notifier')
+                    help='message to display in the notifier')
 
 
 def parse_args(args):
